9325a269840ab38ea7c551712ba67954814c71b6
Hatem Marhraoui <hatem.stage@gmail.com>
2025-11-03 13:57:31 +0100

    compil1

---
+++ compil1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello world\n");
+    return 0;
+}
+
+//.\compil1.c:5:5: error: expected ';' before 'return'
+// il manque le point virgule à la fin de la ligne 4
