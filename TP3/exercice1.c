cdbff4bbac572179c5bc1c9a032c106e3084b954
Hatem Marhraoui <hatem.stage@gmail.com>
2025-11-17 14:59:08 +0100

    exercice1

--- TP3/exercice1.c
+++ TP3/exercice1.c
@@ -32,12 +32,17 @@ int main() {
     img[n-1][j] = '_';
     }
 
-
+ 
     for (int j = mid + n; j < width; j++) {
         img[n-1][j] = '_';
     }
    
-    
+    for (int j = 0; j < width; j++) {
+    if (j == 0 || j == width - 1)
+        img[n][j] = '"';       
+    else
+        img[n][j] = 'S';       
+}
      for(int i=0; i<= max_height; i++){
         for(int j=0; j<=width; j++){
             printf("%c",img[i][j]);
