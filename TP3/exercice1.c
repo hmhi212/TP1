b1f7c65dd6adb2f0a62554947c1db3639d475eb2
Hatem Marhraoui <hatem.stage@gmail.com>
2025-11-17 13:35:53 +0100

    exercice1

--- TP3/exercice1.c
+++ TP3/exercice1.c
@@ -5,10 +5,17 @@ int main() {
     
     printf("Entrez la taille de l'etoile (entre 5 et 10) : ");
     scanf("%d", &n);
+    if (n<5 || n>10){
+        printf(" entreez une taille valide");
+    }
     int width = n * n;
     int max_height = 3* n-1 ;
     char img [11][11] ;
-    
-
+    for (int i=0 ; i<width ; i++){
+        for(int j=0 ; j<max_height ; j++){
+            img[i][j] ='';
+            
+        }
+    }
 }
    
