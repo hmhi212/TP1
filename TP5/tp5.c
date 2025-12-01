8bf9b96d563425eac3b1577aa26a8e98b522482e
Hatem Marhraoui <hatem.stage@gmail.com>
2025-12-01 15:05:53 +0100

    TP5

--- TP5/tp5.c
+++ TP5/tp5.c
@@ -17,13 +17,17 @@ int main()
 
     int consommations[NB_CATEGORIES];
     
-    initialiser(consommations, NB_CATEGORIES);
+    if (charger(consommations, NB_CATEGORIES) == 0) {
+        printf("<< Aucune sauvegarde trouvée >>\n");
+        initialiser(consommations, NB_CATEGORIES);
+    } else {
+        printf("Donnees chargees avec succes.\n");
+    }
 
     int choix = 0;
     while (choix != 3) {
         afficherMenu();
         choix = lireChoix();
-
 
         switch (choix) {
             case 1:
@@ -33,8 +37,11 @@ int main()
                 afficherResume(consommations, NB_CATEGORIES, CATEGORIES, NULL, NULL, NULL, NULL, 0); 
                 break;
             case 3:
-
-                printf("Sauvegarder et quitter...\n");
+                if (sauvegarder(consommations, NB_CATEGORIES) == 1) {
+                    printf("Sauvegarde reussie. Au revoir.\n");
+                } else {
+                    printf("Echec de la sauvegarde. Au revoir.\n");
+                }
                 break;
             default:
                 printf("Choix invalide. Veuillez reessayer.\n");
